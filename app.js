4f6f3fc897f7aaac7bb059a883237feb193ac815
Addi285 <69495986+Addi285@users.noreply.github.com>
2021-04-23 01:31:40 +1000

    major changes to the api calls and data handling

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const content = document.querySelector('.content'); //debug feature
 const confidence = document.querySelector('.confidence'); //debug feature
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-
+var result = "";
+var query = {};
+var nlpEntity = {};
+var locations = [];
+const serverClarif = "http://192.46.220.90:5000/clarif?query=";
+//const serverClarif = "http://192.46.220.90:5000/clarif?query=%22Maximum%20precipitation%20in%20Brisbane%20June%202015%22";    //for testing 
+const serverResponse = "http://192.46.220.90:5000/response?query=%E2%80%9DMaximum%20precipitation%20in%20Brisbane%20June%202015%E2%80%9D&NLPEntity={%22STAT%22:%20[%22maximum%22],%20%22ATTRIBUTE%22:%20[%22precipitation%22],%20%22GPE%22:%20[%22Brisbane%22],%20%22DATE%22:%20[%22June%202015%22]}&locIndex=4";
+//const serverResponse = "http://192.46.220.90:5000/response?query=";
 
 
 //on page refresh or startup
@@ -11,13 +18,14 @@ function startup() {
     document.getElementById("correctQueryButton").style.visibility = "hidden";
     document.getElementById("incorrectQueryButton").style.visibility = "hidden";
     document.getElementById("debug").style.visibility = "hidden";   //debug featuree
+    //response();
 }
 
 function launchChatbot() {
-    speakText("Hello, I am the BOM data handling chatbot. What query would you like me to handle for you?");
+    speakText("Hello. What query would you like me to handle for you?");
     setTimeout(() => {  
         document.getElementById("dictateQueryButton").style.visibility = "visible";
-    }, 6000);
+    }, 3500);
 }
 
 function queryCheck() {
@@ -27,7 +35,72 @@ function queryCheck() {
 }
 
 function correctQuery() {
-    speakText("Wonderful. I will find this out for you.");
+    //speakText("Wonderful. I will find this out for you.");
+    clarifyQuery();
+
+}
+
+function clarifyQuery() {
+    //speakText("Checking my database now.");
+    const theURL = serverClarif + content.textContent;
+    var ajax = new XMLHttpRequest();
+
+    ajax.open("GET", theURL, true);
+    ajax.send(null);
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && (ajax.status == 200)) {
+            console.log("ready")            
+            var Data = JSON.parse(ajax.responseText);
+
+            query = Data.query;
+            console.log("query:", query);
+            nlpEntity = Data.NLPEntity;
+            console.log("NLPEntity:", nlpEntity);
+
+            var i, j;
+            for (i = 0; i < Data.Location.length; i++) {
+                locations[i] = []
+                for (j = 0; j < 1; j++) {
+                    locations[i][j] = Data.Location[i][0];
+                    locations[i][j+1] = Data.Location[i][2];
+                }
+            }
+        } else {
+            console.log("not ready yet")            
+        }
+    }
+    varifyLocation();
+    console.log("SPECIFIC LOCATIONS: ", locations);
+}
+
+function response(locIndex) {
+    //speakText("Excellent. Checking my database again.");
+    const theURL = serverResponse;// + NLPEntity + locIndex;
+    var ajax = new XMLHttpRequest();
+
+    ajax.open("GET", theURL, true);
+    ajax.send(null);
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && (ajax.status == 200)) {
+            console.log("ready")     
+            const Data = JSON.parse(ajax.responseText);       
+            result = Data.Result;
+            console.log("RESULT: ", result);
+            speakText("The answer is:");
+            speakText(result);
+            document.write(result);
+            document.getElementById("debug").style.visibility = "visible";  //debug feature
+        } else {
+            console.log("not ready yet")            
+        }
+    }
+}
+
+function varifyLocation() {
+    //speakText("Please varify the specific location.");
+    
+    response();
+    
 }
 
 function incorrectQuery() {
@@ -68,21 +141,34 @@ function speakText(message) {
     window.speechSynthesis.speak(speech);
 }
 
-
-
-
-
-
-/*
-const Http = new XMLHttpRequest();
-    const url='https://jsonplaceholder.typicode.com/users';
-    //const url='https://mockend.com/Addi285/repo';
-    Http.open("GET", url);
+function httpGet(theUrl) {
+    const Http = new XMLHttpRequest();
+    Http.open("GET", theUrl);
     Http.send();
-     
-    console.log(Http.responseText);
-    var obj = JSON.parse(Http.responseText);
-    document.write(obj[0].title);
-    speakText(obj[0].title);
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+}
+
+function testing() {
     
-*/
+    var url = "http://192.46.220.90:5000/clarif?query=Maximum precipitation in Brisbane June 2015"
+
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", url, true);
+    ajax.send(null);
+    ajax.onreadystatechange = function () {
+
+     if (ajax.readyState == 4 && (ajax.status == 200)) {
+
+        console.log("ready")            
+        var Data = JSON.parse(ajax.responseText);
+        console.log(Data.Location[1]);
+       // console.log(Data.first);
+
+    } else {
+        console.log("not ready yet")            
+    }
+}
+}
